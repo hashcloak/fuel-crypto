8a7df3dd99129fba1bad53632b1e4604044ed13f
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2023-04-03 19:43:35 -0600

    - Merged field and field64 functions. Adapted rest of code to this. - Started with point_arithmetic, not yet working - Started on osswu for hash2curve but not clear how to independently test this

--- p256/src/lib.sw
+++ p256/src/lib.sw
@@ -1,6 +1,8 @@
 library p256;
 
-dep field64;
 dep field;
 dep scalar64;
 dep scalar;
+dep affine;
+dep projective;
+dep point_arithmetic;
--- p256_tests/tests/harness.rs
+++ p256_tests/tests/harness.rs
@@ -293,7 +293,6 @@ let y: Scalar = Scalar{ls:[ 10719928016004921607, 13845646450878251009, 13142370
   assert_eq!(scalar_sub.value.ls[3], 7093558989675447812);
 }
 
-
 #[tokio::test]#[ignore]
 async fn test_scalar_mul() {
   let (_instance, _id) = get_contract_instance().await;
@@ -318,6 +317,7 @@ let y: Scalar = Scalar{ls:[ 10719928016004921607, 13845646450878251009, 13142370
 
 #[tokio::test] #[ignore]
 async fn test_scalar_invert() {
+
   let (_instance, _id) = get_contract_instance().await;
 
   // 6024032581487054615307857608562388818842860057096001857409703737786438595508
@@ -334,4 +334,263 @@ async fn test_scalar_invert() {
   assert_eq!(invert_x.value.value.ls[1], 1325056620123427311);
   assert_eq!(invert_x.value.value.ls[2], 7698614219480972011);
   assert_eq!(invert_x.value.value.ls[3], 13509591698470992260);
+}
+
+// TODO complete test
+#[tokio::test]
+async fn test_proj_double() {
+  let (_instance, _id) = get_contract_instance().await;
+
+  let affine_point = AffinePoint {
+    x: FieldElement{ ls: [12666785159035633708, 8920888101277371171, 11689429229050541387, 4283474778233828756]},
+    y: FieldElement{ ls: [1189565538687329478, 1239862490461304627, 12605683394765277296, 17291001030956535886]},
+    infinity: 0u8
+  };
+
+  let proj_point = _instance
+    .methods()
+    .affine_to_proj(affine_point)
+    .call().await.unwrap();
+
+  let double_point = _instance
+    .methods()
+    .proj_double(proj_point.value)
+    .tx_params(TxParameters::new(None, Some(100_000_000), None))
+    .call().await.unwrap();
+
+  println!("double {:#?}", double_point.value);
+  /*
+    x and y same as `add` function, but z is different. 
+
+double ProjectivePoint {
+    x: FieldElement {
+        ls: [
+            15115213604804018773,
+            14825271215604679363,
+            18209176626028401976,
+            2443548026568239069,
+        ],
+    },
+    y: FieldElement {
+        ls: [
+            6149620218881675608,
+            12704572705094897425,
+            1013366523324741893,
+            10585157331569153230,
+        ],
+    },
+    z: FieldElement {
+        ls: [
+            18077235689792183344,
+            3618401707264396090,
+            4918924327568324049,
+            16756078894964822082,
+        ],
+    },
+}
+  */
+}
+
+// TODO complete test
+#[tokio::test]
+async fn test_proj_add() {
+  let (_instance, _id) = get_contract_instance().await;
+
+  let affine_point_1 = AffinePoint {
+    x: FieldElement{ ls: [12666785159035633708, 8920888101277371171, 11689429229050541387, 4283474778233828756]},
+    y: FieldElement{ ls: [1189565538687329478, 1239862490461304627, 12605683394765277296, 17291001030956535886]},
+    infinity: 0u8
+  };
+  let affine_point_2 = AffinePoint {
+    x: FieldElement{ ls: [12666785159035633708, 8920888101277371171, 11689429229050541387, 4283474778233828756]},
+    y: FieldElement{ ls: [1189565538687329478, 1239862490461304627, 12605683394765277296, 17291001030956535886]},
+    infinity: 0u8
+  };
+
+  let proj_point_1 = _instance
+    .methods()
+    .affine_to_proj(affine_point_1)
+    .call().await.unwrap();
+
+  let proj_point_2 = _instance
+    .methods()
+    .affine_to_proj(affine_point_2)
+    .call().await.unwrap();
+
+  let double_point = _instance
+    .methods()
+    .proj_add(proj_point_1.value, proj_point_2.value)
+    .tx_params(TxParameters::new(None, Some(100_000_000), None))
+    .call().await.unwrap();
+
+  println!("proj add{:#?}", double_point.value);
+  /*
+  x and y same as `double` function, but z is different. 
+
+proj addProjectivePoint {
+    x: FieldElement {
+        ls: [
+            15115213604804018773,
+            14825271215604679363,
+            18209176626028401976,
+            2443548026568239069,
+        ],
+    },
+    y: FieldElement {
+        ls: [
+            6149620218881675608,
+            12704572705094897425,
+            1013366523324741893,
+            10585157331569153230,
+        ],
+    },
+    z: FieldElement {
+        ls: [
+            16379209700525303278,
+            4196642070481431767,
+            7469762217227053229,
+            6707428827071881437,
+        ],
+    },
+}
+  */
+}
+
+// TODO complete test
+#[tokio::test]
+async fn test_mixed_add() {
+  let (_instance, _id) = get_contract_instance().await;
+
+  let affine_point = AffinePoint {
+    x: FieldElement{ ls: [12666785159035633708, 8920888101277371171, 11689429229050541387, 4283474778233828756]},
+    y: FieldElement{ ls: [1189565538687329478, 1239862490461304627, 12605683394765277296, 17291001030956535886]},
+    infinity: 0u8
+  };
+  let affine_point_again = AffinePoint {
+    x: FieldElement{ ls: [12666785159035633708, 8920888101277371171, 11689429229050541387, 4283474778233828756]},
+    y: FieldElement{ ls: [1189565538687329478, 1239862490461304627, 12605683394765277296, 17291001030956535886]},
+    infinity: 0u8
+  };
+
+  let proj_point = _instance
+    .methods()
+    .affine_to_proj(affine_point)
+    .call().await.unwrap();
+
+  let double_point = _instance
+    .methods()
+    .proj_aff_add(proj_point.value, affine_point_again)
+    .tx_params(TxParameters::new(None, Some(100_000_000), None))
+    .call().await.unwrap();
+
+  println!("mixed add {:#?}", double_point.value);
+  /*
+  completely different results than the other 2
+mixed add ProjectivePoint {
+    x: FieldElement {
+        ls: [
+            1510801726910155195,
+            17090838458901703056,
+            6084968679010519560,
+            16405862338188255101,
+        ],
+    },
+    y: FieldElement {
+        ls: [
+            11836891411032767592,
+            3753498121545811263,
+            9306923703222062998,
+            1487259256437579147,
+        ],
+    },
+    z: FieldElement {
+        ls: [
+            4860301594197100376,
+            18441998756991201717,
+            18162882596332682578,
+            4226598687877098654,
+        ],
+    },
+}
+   */
+}
+
+/**
+ * 
+Using GP Pari to test
+
+p = 2^256 - 2^224 + 2^192 + 2^96 - 1;
+a = -3;
+b = 41058363725152142129326129780047268409114441015993725554835256314039467401291;
+E = ellinit([a, b], p);
+U = [Mod(26887806963936644023076993916022178528598206976581727837826133769693683662892, 115792089210356248762697446949407573530086143415290314195533631308867097853951), Mod(108537372577990157610025549686808388768592354808226327274614912068426601145542, 115792089210356248762697446949407573530086143415290314195533631308867097853951)];
+V = elladd(E, U, U);
+print(V);
+
+random(E) (how random point U was generated)
+
+[Mod(26887806963936644023076993916022178528598206976581727837826133769693683662892, 115792089210356248762697446949407573530086143415290314195533631308867097853951), 
+[4283474778233828756, 11689429229050541387, 8920888101277371171, 12666785159035633708]
+FieldElement{ ls: [12666785159035633708, 8920888101277371171, 11689429229050541387, 4283474778233828756]}
+
+Mod(108537372577990157610025549686808388768592354808226327274614912068426601145542, 115792089210356248762697446949407573530086143415290314195533631308867097853951)]
+[17291001030956535886, 12605683394765277296, 1239862490461304627, 1189565538687329478]
+FieldElement{ ls: [1189565538687329478, 1239862490461304627, 12605683394765277296, 17291001030956535886]}
+
+elladd result
+
+[Mod(74981475963429018701923941327168229552755005516444169298501561579389145521924, 115792089210356248762697446949407573530086143415290314195533631308867097853951), 
+ (reverse order) [11945238284211116874, 5617380685649542436, 11264790011350711105, 8189955845634072324]
+
+Mod(71214888696418130043612352873464034692680087082336694427205823938179702081608, 115792089210356248762697446949407573530086143415290314195533631308867097853951)]
+ (reverse order) [11345186313446163181, 4471073799328713103, 7167355551294680272, 3629897320574371912]
+
+ This is completely different than the double, add, and mixadd results....
+ * */ 
+
+ #[tokio::test]#[ignore = "already tested"]
+async fn test_affine_to_proj() {
+  let (_instance, _id) = get_contract_instance().await;
+
+  let random_affine_point = AffinePoint {
+    x: FieldElement{ ls: [12666785159035633708, 8920888101277371171, 11689429229050541387, 4283474778233828756]},
+    y: FieldElement{ ls: [1189565538687329478, 1239862490461304627, 12605683394765277296, 17291001030956535886]},
+    infinity: 0u8
+  };
+
+  let proj_point = _instance
+    .methods()
+    .affine_to_proj(random_affine_point)
+    .call().await.unwrap();
+
+  println!("mixed add {:#?}", proj_point.value);
+  /*
+  correct 
+  mixed add ProjectivePoint {
+    x: FieldElement {
+        ls: [
+            12666785159035633708,
+            8920888101277371171,
+            11689429229050541387,
+            4283474778233828756,
+        ],
+    },
+    y: FieldElement {
+        ls: [
+            1189565538687329478,
+            1239862490461304627,
+            12605683394765277296,
+            17291001030956535886,
+        ],
+    },
+    z: FieldElement {
+        ls: [
+            1,
+            0,
+            0,
+            0,
+        ],
+    },
+}
+   */
 }
